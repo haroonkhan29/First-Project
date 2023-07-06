81a8d692dfcd3d4d9a6d834826450fd98f29b521
Haroon Afridi <haroon77.afridi@gmail.com>
2023-07-06 21:13:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,113 @@
+// // // // // const arr =  [3,5,6,6,7,8,9]
+// // // // // const name = false
+// // // // // console.log(typeof name)
+
+// // // // // const arr = [3,4,5,6,7]
+// // // // // arr.push(10)  // add at the end of array
+// // // // // arr.unshift(20)  // add at the start of array
+// // // // // arr.pop() // remove from end
+// // // // // arr.pop() // remove from end
+// // // // // arr.shift()
+// // // // // arr.shift()
+
+// // // // //  function
+// // // // // function hello(){
+// // // // //     console.log('function created')
+// // // // // }
+// // // // // const hello = () => console.log('hello from arrow 22')
+// // // // // const greet = (name) =>{
+// // // // //     // console.log('hello ' + name)
+// // // // //     // console.log(`hello ${name}`)
+// // // // // }
+// // // // //  greet('haroon')
+// // // // //  greet('sami')
+
+// // // // // const sum = (a,b) =>{
+// // // // //    return a+b;
+// // // // // }
+// // // // //  const res  =  sum(3,4)
+// // // // //  console.log(res)
+
+// // map
+// // const arr = [3,4,5,6,7]
+// // let sum  = 0;
+// // arr.map(a=> {
+// //    console.log(arr.length);
+// // })
+
+// // // // // objects
+
+// // // // // let user = {
+// // // // //     email:'sami@gmail.com',
+// // // // //     password:'samikhan',
+// // // // //     age:12
+// // // // // }
+
+// // // // // console.log(user.email)
+
+
+// // // // // console.log(arr)
+
+// // // // // const posts = [
+// // // // //     {name:'samii khan', age:12},
+// // // // //     {name:"haroon", age:21},
+// // // // //     {name:'tahmeed', age:31}
+// // // // // ]
+// // // // // posts.map(user=> onsocle.log(`Name is ${user.name} and age is ${user.age}`))
+
+// // // // // array destructuring, object, spread, rest operator, reducer will study in react
+
+
+
+// // // // let name = 'Haroon'
+// // // // function demo()
+// // // // {
+// // // //     let name = 'haroon'
+// // // //     console.log
+// // // // }
+// // // // demo()
+// // // // console.log(name)
+
+// // // const arr = [2 , 3 , 5 , 7 ]
+// // // console.log(arr[2])
+
+
+// // const arr = [2 , 3 , 5 , 3 , 5]
+// // arr.map((number)=>{
+// //   console.log(  number + 2)
+// // }) 
+// var x = "John Doe";
+// console.log(x)
+// var x = 0;
+// console.log(x)
+
+// const vehicles = ['mustang', 'f-150', 'expedition']
+// const [car, truck, suv] = vehicles;
+// console.log()
+// const vehicles = ['mustang', 'f-150', 'expedition'];
+
+// const [car,, suv] = vehicles;
+// console.log(vehicles)
+
+// const numbersOne = [1, 2, 3];
+// const numbersTwo = [4, 5, 6];
+// const numbersCombined = [...numbersOne, ...numbersTwo];
+// console.log(`The first is ${numbersOne} and ${numbersTwo}`)
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const [one, two, ...rest] = numbers;
+// console.log(numbers)
+
+// const bike = [1,2,3,4,5,6,7];
+//   const [a,b,c, ...others] = bike;
+
+//   const newbike = [...bike, 8,9]
+
+
+//   console.log(newbike)
+// const isAdmin = false;
+// isAdmin && console.log('hello admin')
+
+// const isAdmin = false;
+// !isAdmin  ? console.log('Not') : console.log('admin')
